3cc2289d742fa9c22c16f011a80f15beed7d9434
Anonymous <>
2021-09-26 13:21:19 +0100

    Initial commit - Setting up the project

---
+++ src/test/java/com/digitalstork/tennisgamekata/TennisGameKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalstork.tennisgamekata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisGameKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
