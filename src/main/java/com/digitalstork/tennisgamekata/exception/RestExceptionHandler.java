783bf49d43997eb8e56b35c999f502e9a548a691
Anonymous <>
2021-09-27 01:50:00 +0100

    Implement UnauthorizedActionException handling

--- src/main/java/com/digitalstork/tennisgamekata/exception/RestExceptionHandler.java
+++ src/main/java/com/digitalstork/tennisgamekata/exception/RestExceptionHandler.java
@@ -6,10 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,5 +38,18 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
                 .build();
 
         return new ResponseEntity<>(apiError, headers, status);
+    }
+
+    @ExceptionHandler(UnauthorizedActionException.class)
+    protected ResponseEntity<Object> handleUnauthorizedActionException(UnauthorizedActionException ex) {
+        ApiError apiError = ApiError.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST)
+                .errorCode(ErrorCode.UNAUTHORIZED_ACTION.name())
+                .message(ex.getMessage())
+                .subErrors(new ArrayList<>())
+                .build();
+
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 }
